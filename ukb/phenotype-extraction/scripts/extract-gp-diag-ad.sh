fff01135750d8bb5834aad6a8e411f23e0b31976
Thomas Battram <thomas.battram@bristol.ac.uk>
2022-12-21 11:04:26 +0000

    altered so it could be run manually

--- ukb/phenotype-extraction/scripts/extract-gp-diag-ad.sh
+++ ukb/phenotype-extraction/scripts/extract-gp-diag-ad.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sbatch <<EOT
-#!/bin/bash
 
 #SBATCH --job-name=extract-gp-dat
 #SBATCH --partition=veryshort
@@ -18,10 +16,12 @@ sbatch <<EOT
 ## diagnosed by their GP
 
 ## args
-V2_LIST=$1
-CTV3_LIST=$2
-GP_FILE=$3
-OUT=$4
+# V2_LIST=$1
+# CTV3_LIST=$2
+# GP_FILE=$3
+# OUT=$4
+
+## add manual args here!
 
 dos2unix ${V2_LIST}
 dos2unix ${CTV3_LIST}
@@ -32,5 +32,3 @@ awk -F"\t" 'F==1{a2[$1]=$1}F==2{a3[$1]=$1}F==3{if($4 in a2 || $5 in a3){print $1
  > ${OUT}
 
 sort -u ${OUT} > ${OUT}.uniq
-
-EOT
