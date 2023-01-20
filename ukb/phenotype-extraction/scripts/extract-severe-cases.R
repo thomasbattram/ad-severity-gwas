9954979627fdead5c80b0d5ac7e5a4bec92933cb
Thomas Battram <thomas.battram@bristol.ac.uk>
2023-01-20 18:43:37 +0000

    updated to include new definitions and new data

--- ukb/phenotype-extraction/scripts/extract-severe-cases.R
+++ ukb/phenotype-extraction/scripts/extract-severe-cases.R
@@ -8,6 +8,7 @@
 ## pkgs
 library(data.table)
 library(tidyverse)
+library(openxlsx)
 
 ## args
 args <- commandArgs(trailingOnly = TRUE)
@@ -26,11 +27,13 @@ id_file <- "data/ukb-pheno/ids.txt"
 ad_id_file <- "data/ad-ids.txt"
 gp_scripts_id_file <- "data/ukb-pheno/gp_scripts_ids.txt" ## CHANGE ME
 phototherapy_file <- "data/ukb-pheno/phototherapy.txt"
-treatments_file <- "data/ukb-pheno/treatments.txt"
+treatments_file <- "data/ukb-pheno/summ-sr-treatment-data-clean.tsv"
 icd10_file <- "data/ukb-pheno/icd10.txt"
 icd9_file <- "data/ukb-pheno/icd9.txt"
-summary_outfile <- "data/severity-counts.tsv" 
+summary_outfile <- "data/severity-counts.xlsx" 
 data_outfile <- "data/case-pheno.tsv"
+gp_file <- "data/ukb-pheno/summ-gp-script-data-clean.tsv"
+
 
 # gp_record_file <- "data/ukb-pheno/gp-records.txt"
 # gp_record <- fread(gp_record_file)
@@ -38,11 +41,12 @@ data_outfile <- "data/case-pheno.tsv"
 ## data
 ids <- fread(id_file)
 ad_ids <- readLines(ad_id_file)
+gp_ids <- readLines(gp_scripts_id_file)
 pt_dat <- fread(phototherapy_file)
-treat_dat <- fread(treatments_file)
+treat_dat <- read_tsv(treatments_file)
 icd10_dat <- fread(icd10_file)
 icd9_dat <- fread(icd9_file)
-gp_scripts_ids <- readLines(gp_scripts_id_file)
+gp_dat <- read_tsv(gp_file)
 ## NOTE: currently these files = ~2Gb 
 
 # ------------------------------------------------------------------------
@@ -95,6 +99,48 @@ get_n <- function(count_dat, variables)
 	return(n_dat)
 }
 
+#' get number of cases for a given definition of severe AD
+#' 
+#' @param count_dat combined combination of counts for each variable of interest - comb_count
+#' @param systemics vector of systemic treatment names
+#' @param topicals default = NULL. vector of topical steroid variables
+#' @param other_treat default = NULL. list of other treatments, e.g. phototherapy treatments, ICD9 codes, ICD10 codes
+#' @return tibble giving the numbers across each treatment and the unique cases 
+get_definition_n <- function(count_dat, systemics, topicals = NULL, other_treat = NULL)
+{
+	gp_def <- paste0(c(systemics, topicals), "_gp")
+	sr_def <- paste0(c(systemics, topicals), "_sr")
+	all_vars <- c(gp_def, sr_def, unlist(other_treat, use.names = FALSE))
+	count_def <- count_dat %>%
+		dplyr::select(f.eid, all_of(all_vars)) %>%
+		mutate(severe = if_else(if_any(-f.eid, ~ . > 0), 1, 0))
+	def_n <- get_n(count_def, all_vars)
+	no_cases_tab <- dplyr::filter(def_n, !grepl("cases", variable))
+	## Remove gp and sr from vars
+	no_cases_tab <- no_cases_tab %>%
+		mutate(`Data type` = case_when(variable %in% gp_def ~ "GP prescription",
+									   variable %in% sr_def ~ "Self-report")) %>%
+		mutate(variable = gsub("_gp|_sr", "", variable))
+	sort_tab <- map_dfr(1:nrow(no_cases_tab), function(i) {
+		temp_tab <- no_cases_tab[i, ]
+		v <- temp_tab$variable
+		if (v %in% systemics) temp_tab[["General phenotype"]] <- "Systemics"
+		if (v %in% topicals) temp_tab[["General phenotype"]] <- "Topicals"
+		if (v %in% other_treat$icd9) temp_tab[["General phenotype"]] <- "Primary diagnosis - ICD9 code"
+		if (v %in% other_treat$icd10) temp_tab[["General phenotype"]] <- "Primary diagnosis - ICD10 code"
+		if (v %in% other_treat$photo) temp_tab[["General phenotype"]] <- "Phototherapy"
+		return(temp_tab)
+	})
+	sort_tab <- sort_tab %>%
+		arrange(`General phenotype`, variable)
+	cases_tab <- def_n %>%
+		dplyr::filter(grepl("cases", variable)) %>%
+		mutate(`Data type` = NA, `General phenotype` = NA)
+	out_tab <- bind_rows(sort_tab, cases_tab) %>%
+		dplyr::select(`General phenotype`, variable, `Data type`, n)
+	return(out_tab)
+}
+
 # ------------------------------------------------------------------------
 # Phototherapy
 # ------------------------------------------------------------------------
@@ -125,46 +171,18 @@ rm(pt_dat)
 # Systemic treatments
 # ------------------------------------------------------------------------
 
-# mtx - methotrexate (1140910036)
-# methotrexate (1140869848)
-# dupilumab isn't present for some reason
+sr_drugs <- colnames(treat_dat)[colnames(treat_dat) != "f.eid"]
+colnames(treat_dat)[colnames(treat_dat) != "f.eid"] <- paste0(sr_drugs, "_sr")
 
-sys_meta <- tibble(field_id = 20003, 
-				   code = c(1140909844, 1141181020, 1140909864, 1140869930, 1140925978, 1140910036, 1140869848), 
-				   meaning = c("ciclosporin",
-				   			   "ciclosporin product", 
-				   			   "azt - azathioprine", 
-				   			   "azathioprine", 
-				   			   "mycophenolate", 
-				   			   "mtx - methotrexate", 
-				   			   "methotrexate"))
+sr_treat_count <- ids %>%
+	left_join(treat_dat)
 
-sys_treat_vars <- c(1140909844, 1141181020, 1140909864, 1140869930, 1140925978, 1140910036, 1140869848)
-sys_treat_count <- count_pheno(ids, treat_dat, sys_treat_vars)
-sys_n <- get_n(sys_treat_count, sys_treat_vars)
+sr_treat_count[sr_treat_count == TRUE] <- 1
+sr_treat_count <- as_tibble(sr_treat_count)
 
-# ------------------------------------------------------------------------
-# Corticosteroid treatments
-# ------------------------------------------------------------------------
-
-cort_meta <- tibble(field_id = 20003, 
-					code = c(1140884696, 1141157294, 1140874896, 1140910424, 1141189464, 1140909786, 1140874790, 
-			   				 1140888074, 1140888098, 1140888172),
-					meaning = c("clobetasone", 
-								"hydrocortisone product",
-								"hydrocortisone", 
-								"hc - hydrocortisone", 
-								"exe-cort hydrocortisone 1% cream", 
-								"beclometasone", 
-								"Betamethasone", 
-								"clobetasol", 
-								"fluticasone", 
-								"mometasone"))
-
-cort_vars <- c(1140884696, 1141157294, 1140874896, 1140910424, 1141189464, 1140909786, 1140874790, 
-			   1140888074, 1140888098, 1140888172)
-cort_count <- count_pheno(ids, treat_dat, cort_vars)
-cort_n <- get_n(cort_count, cort_vars)
+## Sanity check
+sum(treat_dat$Methotrexate_sr)
+sum(sr_treat_count$Methotrexate_sr, na.rm = T)
 
 # ------------------------------------------------------------------------
 # ICD 10 codes
@@ -197,39 +215,93 @@ icd9_n <- get_n(icd9_count, icd9_codes)
 # ------------------------------------------------------------------------
 
 ## This is done in another script, just putting it into the same format as the others
+gp_drugs <- colnames(gp_dat)[colnames(gp_dat) != "f.eid"]
+colnames(gp_dat)[colnames(gp_dat) != "f.eid"] <- paste0(gp_drugs, "_gp")
 
-# gp_scripts_ids <- sample(ids$f.eid, 1000) # TESTER
+gp_treat_count <- ids %>%
+	left_join(gp_dat)
 
-gps_count <- tibble(f.eid = ids$f.eid) %>%
-			  	mutate(gp_diagnosis = ifelse(f.eid %in% gp_scripts_ids, 1, 0)) 
+gp_treat_count[gp_treat_count == TRUE] <- 1
+gp_treat_count <- as_tibble(gp_treat_count)
+
+## For those with missing data, put as 0
+gp_treat_count <- gp_treat_count %>%
+	dplyr::filter(f.eid %in% ad_ids) 
+gp_treat_count[is.na(gp_treat_count)] <- 0
+
+## Sanity check
+sum(gp_dat$Methotrexate_gp)
+sum(gp_treat_count$Methotrexate_gp, na.rm = T)
 
 # ------------------------------------------------------------------------
 # Combine data and write it out
 # ------------------------------------------------------------------------
 
 ## Quick combination of numbers 
-comb_count <- list(pt_count, sys_treat_count, icd10_count, icd9_count, gps_count) %>%
+comb_count <- list(pt_count, sr_treat_count, gp_treat_count, icd10_count, icd9_count) %>%
 	reduce(left_join) %>%
-	dplyr::filter(f.eid %in% ad_ids) %>%
-	mutate(severe = if_else(if_any(-f.eid, ~ . > 0), 1, 0)) # asking if any are present then == 1, otherwise 0
+	dplyr::filter(f.eid %in% gp_ids, f.eid %in% ad_ids)
+	# mutate(severe = if_else(if_any(-f.eid, ~ . > 0), 1, 0)) # asking if any are present then == 1, otherwise 0
 
-comb_vars <- c(photo_vars, sys_treat_vars, icd10_codes, icd9_codes, "gp_diagnosis")
-comb_n <- get_n(comb_count, comb_vars)
+## Check numbers for each variety of severity definition
+sys <- c("Methotrexate", "Ciclosporin", "Azathioprine", "Mycophenolate")
+top1 <- c("Topical steroids", "Topical calcineurin inhibitors")
+top2 <- c("Topical coal tar", "Topical zinc", "Topical ichthammol")
+top3 <- c("Super potent steroids")
+top4 <- c("Potent steroids")
+top5 <- c("Super potent steroids x2")
+top6 <- c("Potent steroids x2")
 
-comb_meta <- list(pt_meta, sys_meta, icd10_meta, icd9_meta) %>%
-	map(mutate_all, as.character) %>%
-	bind_rows()
-	
-comb_meta_out <- comb_n %>%
-	left_join(comb_meta, by = c("variable" = "code")) %>%
-	dplyr::select(variable, n, ukb_code_meaning = meaning, ukb_field_id = field_id)
+## Definition 1: systemics
+def1_n <- get_definition_n(comb_count, sys)
 
-write.table(comb_meta_out, file = summary_outfile, col.names = T, row.names = F, quote = F, sep = "\t")
+## Definition 2: systemics or topical steroids or Topical calcineurin inhibitors
+def2_n <- get_definition_n(comb_count, sys, top1)
 
-## Combination with corticosteroids
+## Definition 3: def 2 or Coal tar OR Topical zinc OR Ichthammol
+def3_n <- get_definition_n(comb_count, sys, c(top1, top2))
+
+## Definition 4: def 1 or ICD9 or ICD10 or phototherapy
+other_treat <- list(photo = photo_vars, icd9 = icd9_codes, icd10 = icd10_codes)
+def4_n <- get_definition_n(comb_count, sys, other_treat = other_treat)
+
+## Definition 5: systemics or super potent topicals
+def5_n <- get_definition_n(comb_count, sys, top3)
+
+## Definition 6: systemics or super potent topicals or potent topicals
+def6_n <- get_definition_n(comb_count, sys, c(top3, top4))
+
+## Definition 7: systemics or super potent topicals x2
+def7_n <- get_definition_n(comb_count, sys, top5)
+
+## Definition 8: systemics or potent topicals x2 (or super potent topicals)
+def8_n <- get_definition_n(comb_count, sys, top6)
+
+def_list <- list(d1 = def1_n, d2 = def2_n, d3 = def3_n, d4 = def4_n, 
+				 d5 = def5_n, d6 = def6_n, d7 = def7_n, d8 = def8_n)
+
+## Checking concordance between self-report and GP prescriptions
+gp_def1 <- paste0(sys, "_gp")
+sr_def1 <- paste0(sys, "_sr")
+count_def1 <- comb_count %>%
+	dplyr::select(f.eid, all_of(c(gp_def1, sr_def1))) %>%
+	mutate(severe = if_else(if_any(-f.eid, ~ . > 0), 1, 0))
+
+sum(count_def1$Methotrexate_sr == 1 & count_def1$Methotrexate_gp == 1) # 143
+sum(count_def1$Ciclosporin_sr == 1 & count_def1$Ciclosporin_gp == 1) # 12
+sum(count_def1$Azathioprine_sr == 1 & count_def1$Azathioprine_gp == 1) # 10
+sum(count_def1$Mycophenolate_sr == 1 & count_def1$Mycophenolate_gp == 1) # 13
 
 
-
+## QUICK AND MESSY WRITE UP
+wb <- createWorkbook()
+lapply(seq_along(def_list), function(x) {
+	nam <- names(def_list)[x]	
+	addWorksheet(wb, sheetName = nam)
+	writeData(wb, sheet = nam, x = def_list[[nam]],
+				   colNames = TRUE, rowNames = FALSE)
+})
+saveWorkbook(wb, summary_outfile, overwrite = TRUE)
 
 
 
