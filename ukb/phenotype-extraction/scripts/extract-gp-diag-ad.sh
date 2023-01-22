ba9959732244442f15fbb383c7c1cdbee5ffe8d2
Thomas Battram <thomas.battram@bristol.ac.uk>
2023-01-22 11:49:20 +0000

    added manual args

--- ukb/phenotype-extraction/scripts/extract-gp-diag-ad.sh
+++ ukb/phenotype-extraction/scripts/extract-gp-diag-ad.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#SBATCH --job-name=extract-gp-dat
-#SBATCH --partition=veryshort
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=4:00:00 
-#SBATCH --mem=16GB
-
 # ---------------------------------------------------------------------------
 # Extract UK Biobank AD cases from GP records
 # ---------------------------------------------------------------------------
@@ -15,13 +7,21 @@
 ## This script extracts all UK Biobank IDs that have read codes corresponding to AD as 
 ## diagnosed by their GP
 
+## Probably best to run this script manually on a compute node 
+## using the command below and tmux to prevent timing out
+# srun --job-name "InteractiveJob" --nodes=1 --ntasks-per-node=1 --cpus-per-task=1 --time=4:00:00 --mem=16GB --pty bash
+
 ## args
 # V2_LIST=$1
 # CTV3_LIST=$2
 # GP_FILE=$3
 # OUT=$4
 
-## add manual args here!
+## manual args
+V2_LIST="AD_v2.list"
+CTV3_LIST="AD_ctv3.v2.uniq.list"
+GP_FILE="gp_clinical.txt"
+OUT="gp-diag-ad-case-eids.txt"
 
 dos2unix ${V2_LIST}
 dos2unix ${CTV3_LIST}
