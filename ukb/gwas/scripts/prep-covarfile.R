60a9a2ba813fe6cef9e91cbb5fafa7d5be3ed396
Thomas Battram <thomas.battram@bristol.ac.uk>
2023-03-05 16:28:02 +0000

    updated for using plink

--- ukb/gwas/scripts/prep-covarfile.R
+++ ukb/gwas/scripts/prep-covarfile.R
@@ -12,6 +12,8 @@
 
 ## pkgs
 library(tidyverse) # tidy code and data
+library(usefunc) # own package of useful functions - used for is.binary()
+
 
 ## args
 args <- commandArgs(trailingOnly = TRUE)
@@ -20,13 +22,18 @@ pc_file <- args[2]
 age_file <- args[3]
 linker_file <- args[4]
 outfile <- args[5]
+plink <- args[6]
 
 ## manual args
 # pheno_file <- "data/severe-ad-phenofile.txt"
-# pc_file <- "UKB_GEN_DIR/derived/principal_components/data.pca1-10.plink.txt"
+# UKB_GEN_DIR <- ""
+# pc_file <- file.path(UKB_GEN_DIR, "derived/principal_components/data.pca1-10.plink.txt")
 # age_file <- "../phenotype-extraction/data/ukb-pheno/age-sex.txt"
 # linker_file <- "data/linker.csv"
 # outfile <- "data/severe-ad-covarfile.txt"
+# plink <- TRUE
+
+plink <- as.logical(plink)
 
 ## data
 pheno_dat <- read_delim(pheno_file, delim = " ")
@@ -56,6 +63,16 @@ covar_dat <- pc_dat %>%
 	left_join(sorted_age) %>%
 	dplyr::select(FID, IID, all_of(paste0("PC", 1:10)), sex, age) %>%
 	dplyr::filter(FID %in% pheno_dat$FID)
+
+## Change binary variables to being a 1/2 rather than a 0/1 because plink is stupid
+if (plink) {
+	bin_vars <- sapply(covar_dat, is.binary)
+	bin_vars <- names(bin_vars)[bin_vars]
+	for (bin in bin_vars) {
+		covar_dat[[bin]] <- replace(covar_dat[[bin]], covar_dat[[bin]] == 1, 2)
+		covar_dat[[bin]] <- replace(covar_dat[[bin]], covar_dat[[bin]] == 0, 1)
+	}
+}
 
 write.table(covar_dat, file = outfile, col.names = T, row.names = F, quote = F, sep = " ")
 
--- ukb/gwas/scripts/prep-phenofile.R
+++ ukb/gwas/scripts/prep-phenofile.R
@@ -12,6 +12,7 @@
 
 ## pkgs
 library(tidyverse) # tidy code and data
+library(usefunc) # own package of useful functions -- for is.binary()
 
 ## args
 args <- commandArgs(trailingOnly = TRUE)
@@ -20,28 +21,36 @@ severe_def <- args[2]
 linker_file <- args[3]
 exclusions_file <- args[4]
 exc <- args[5]
-outfile <- args[6]
+withdrawl_file <- args[6]
+outfile <- args[7]
+plink <- args[8]
 
 ## manual args
 # severe_file <- "../phenotype-extraction/data/case-pheno.tsv"
-# severe_def <- "d4"
+# severe_def <- "d9"
 # linker_file <- "data/linker.csv"
 # exclusions_file <- "data/ukb-exclusions.tsv"
 # exc <- "sex_mismatch sex_chr_aneuploidy het_miss_outliers" ## ADD IN EXCLUSIONS FROM LIST 
-# outfile <- "data/severe-ad-phenofile.txt"
+# withdrawl_file <- "data/w15147_20210809.csv"
+# outfile <- "data/mod_severe-ad-phenofile.txt"
+# plink <- "TRUE"
+
+plink <- as.logical(plink)
 
 ## data
 severe_dat <- read_tsv(severe_file)
 linker <- read_csv(linker_file)
 exc <- unlist(str_split(exc, " "))
+withdrawals <- readLines(withdrawl_file)
 exclusions <- read_tsv(exclusions_file) %>%
 	dplyr::filter(exc_reason %in% exc)
 
 # ------------------------------------------------------------------------------
-# remove exclusions
+# remove exclusions and withdrawals
 # ------------------------------------------------------------------------------
 
 severe_no_exc <- severe_dat %>%
+	dplyr::filter(!f.eid %in% withdrawals) %>%
 	left_join(linker, by = c("f.eid" = "app")) %>%
 	dplyr::filter(!ieu %in% exclusions$id)
 
@@ -56,6 +65,16 @@ nrow(severe_dat) - nrow(severe_no_exc)
 severe_only <- severe_no_exc %>%
 	dplyr::select(FID = ieu, IID = ieu, one_of(severe_def))
 colnames(severe_only)[3] <- "severe"
+
+## Change binary variables to being a 1/2 rather than a 0/1 because plink is stupid
+if (plink) {
+	bin_vars <- sapply(severe_only, is.binary)
+	bin_vars <- names(bin_vars)[bin_vars]
+	for (bin in bin_vars) {
+		severe_only[[bin]] <- replace(severe_only[[bin]], severe_only[[bin]] == 1, 2)
+		severe_only[[bin]] <- replace(severe_only[[bin]], severe_only[[bin]] == 0, 1)
+	}
+}
 
 ## write out the data
 write.table(severe_only, file = outfile, col.names = T, row.names = F, quote = F, sep = " ")
