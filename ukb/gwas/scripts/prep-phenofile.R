fab86c7e13ae7e9508d0e8d97b12e3237494177e
Thomas Battram <thomas.battram@bristol.ac.uk>
2023-03-14 14:14:55 +0000

    updates to deal with using plink

--- ukb/gwas/scripts/prep-phenofile.R
+++ ukb/gwas/scripts/prep-phenofile.R
@@ -30,10 +30,10 @@ plink <- args[8]
 # severe_def <- "d9"
 # linker_file <- "data/linker.csv"
 # exclusions_file <- "data/ukb-exclusions.tsv"
-# exc <- "sex_mismatch sex_chr_aneuploidy het_miss_outliers" ## ADD IN EXCLUSIONS FROM LIST 
+# exc <- "sex_mismatch sex_chr_aneuploidy het_miss_outliers non_europeans" ## ADD IN EXCLUSIONS FROM LIST 
 # withdrawl_file <- "data/w15147_20210809.csv"
 # outfile <- "data/mod_severe-ad-phenofile.txt"
-# plink <- "TRUE"
+# plink <- "FALSE"
 
 plink <- as.logical(plink)
 
@@ -56,6 +56,25 @@ severe_no_exc <- severe_dat %>%
 
 nrow(severe_dat) - nrow(severe_no_exc)
 ## 116 people removed 
+
+## Write out exclusions list in eids for Jake
+# eids_exc <- exclusions %>%
+# 	left_join(linker, by = c("id" = "ieu")) %>%
+# 	pull(app) %>%
+# 	unique()
+
+# eids_exc <- unique(eids_exc, withdrawals)
+
+# writeLines(as.character(eids_exc), "data/ukb-non-eur-exclusion-list.txt")
+
+## all eids
+## Size of fam file = 488377 = size of linker file
+# eids_used <- linker %>%
+# 	dplyr::filter(!app %in% withdrawals, 
+# 				  !ieu %in% exclusions$id) %>%
+# 	pull(app)
+
+# writeLines(as.character(eids_used), "data/ukb-non-eur-inclusion-list.txt")
 
 # ------------------------------------------------------------------------------
 # extract severe phenotype
@@ -80,10 +99,40 @@ if (plink) {
 write.table(severe_only, file = outfile, col.names = T, row.names = F, quote = F, sep = " ")
 
 
+# ## CHECKING NUMBERS
+# def_exc <- c("sex_mismatch", "sex_chr_aneuploidy", "het_miss_outliers")
+# other_exc <- c("minimal_relateds", "non_europeans")
 
+# exclusions_def <- exclusions %>%
+# 	dplyr::filter(exc_reason %in% def_exc)
 
+# ## Expected exclusions
+# severe_no_def_exc <- severe_dat %>%
+# 	dplyr::filter(!f.eid %in% withdrawals) %>%
+# 	left_join(linker, by = c("f.eid" = "app")) %>%
+# 	dplyr::filter(!ieu %in% exclusions_def$id)
 
+# table(severe_no_def_exc$d4) # severe: 605 cases, 29646 controls
+# table(severe_no_def_exc$d9) # mod-severe: 6125 cases, 24126 controls
 
+# ## Minimal related exclusions
+# severe_norel <- severe_no_def_exc %>%
+# 	dplyr::filter(!ieu %in% exclusions[exclusions$exc_reason == "minimal_relateds", ]$id)
 
+# table(severe_norel$d4) # severe: 509 cases, 24754 controls
+# table(severe_norel$d9) # mod-severe: 5091 cases, 20172 controls
 
+# ## Non-European exclusions
+# severe_noeur <- severe_no_def_exc %>%
+# 	dplyr::filter(!ieu %in% exclusions[exclusions$exc_reason == "non_europeans", ]$id)
+
+# table(severe_noeur$d4) # severe: 582 cases, 28353 controls
+# table(severe_noeur$d9) # mod-severe: 5794 cases, 23141 controls
+
+# ## Minimal related and Non-European exclusions
+# severe_norel_eur <- severe_no_def_exc %>%
+# 	dplyr::filter(!ieu %in% exclusions[exclusions$exc_reason %in% other_exc, ]$id)
+
+# table(severe_norel_eur$d4) # severe: 490 cases, 23568 controls
+# table(severe_norel_eur$d9) # mod-severe: 4788 cases, 19270 controls
 
